6f90467d25d8eea926d6745d8cc1f9a5ee8bee54
Skuen <tuananhxvtm@gmail.com>
2024-04-02 09:20:17 +0700

    [Create] Kho_User Page

---
+++ TKS_Mock_Data_Access/Controller/CDM_Kho_User_Controller.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKS_Mock_Data_Access.DataLayer;
+using TKS_Mock_Data_Access.Entity;
+using TKS_Mock_Data_Access.Utility;
+
+namespace TKS_Mock_Data_Access.Controller
+{
+    public class CDM_Kho_User_Controller
+    {
+        public List<CDM_Kho_User> FQ_117_KU_sp_sel_List_For_Cache()
+        {
+            List<CDM_Kho_User> v_arrRes = new List<CDM_Kho_User>();
+            DataTable v_dt = new DataTable();
+
+            try
+            {
+                CSqlHelper.FillDataTable(CConfig.TKS_Mock_Conn_String, v_dt, "FQ_117_KU_sp_sel_List_For_Cache");
+
+                foreach (DataRow v_row in v_dt.Rows)
+                {
+                    CDM_Kho_User v_objRes = CUtility.Map_Row_To_Entity<CDM_Kho_User>(v_row);
+                    v_arrRes.Add(v_objRes);
+                }
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                v_dt.Dispose();
+            }
+
+            return v_arrRes;
+        }
+        public CDM_Kho_User FQ_117_KU_sp_sel_Get_By_ID(string p_iID)
+        {
+            CDM_Kho_User v_objRes = null;
+            DataTable v_dt = new DataTable();
+
+            try
+            {
+                CSqlHelper.FillDataTable(CConfig.TKS_Mock_Conn_String, v_dt, "FQ_117_KU_sp_sel_Get_By_ID", p_iID);
+
+                if (v_dt.Rows.Count > 0)
+                {
+                    v_objRes = CUtility.Map_Row_To_Entity<CDM_Kho_User>(v_dt.Rows[0]);
+                }
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                v_dt.Dispose();
+            }
+
+            return v_objRes;
+        }
+        public int FQ_117_KU_sp_ins_Insert(CDM_Kho_User p_objData)
+        {
+            int v_iRes = CConst.INT_VALUE_NULL;
+
+            try
+            {
+                v_iRes = CSqlHelper.ExecuteNonquery(CConfig.TKS_Mock_Conn_String, "FQ_117_KU_sp_ins_Insert",
+                    p_objData.Ma_Dang_Nhap, p_objData.Kho_ID);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+            return v_iRes;
+        }
+        public int FQ_117_KU_sp_upd_Update(CDM_Kho_User p_objData)
+        {
+            int v_iRes = CConst.INT_VALUE_NULL;
+            try
+            {
+                v_iRes = CSqlHelper.ExecuteNonquery(CConfig.TKS_Mock_Conn_String, "FQ_117_KU_sp_upd_Update",
+                    p_objData.Ma_Dang_Nhap, p_objData.Kho_ID);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+            return v_iRes;
+        }
+        public int FQ_117_KU_sp_del_Delete_By_ID(string p_iD)
+        {
+            int v_iRes = CConst.INT_VALUE_NULL;
+            try
+            {
+                v_iRes = CSqlHelper.ExecuteNonquery(CConfig.TKS_Mock_Conn_String, "FQ_117_KU_sp_del_Delete_By_ID", p_iD);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+            return v_iRes;
+        }
+    }
+}
---
+++ TKS_Mock_Data_Access/Entity/CDM_Kho_User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKS_Mock_Data_Access.Utility;
+
+namespace TKS_Mock_Data_Access.Entity
+{
+    public class CDM_Kho_User
+    {
+        private string m_strMa_Dang_Nhap;
+        private string m_strKho_ID;
+
+        public CDM_Kho_User()
+        {
+            ResetData();
+        }
+
+        public void ResetData()
+        {
+            m_strMa_Dang_Nhap = CConst.STR_VALUE_NULL;
+            m_strKho_ID = CConst.STR_VALUE_NULL;
+        }
+
+        public string Ma_Dang_Nhap
+        {
+            get
+            {
+                return m_strMa_Dang_Nhap;
+            }
+            set
+            {
+                m_strMa_Dang_Nhap = value.Trim();
+            }
+        }
+
+        public string Kho_ID
+        {
+            get
+            {
+                return m_strKho_ID;
+            }
+            set
+            {
+                m_strKho_ID = value.Trim();
+            }
+        }
+    }
+}
